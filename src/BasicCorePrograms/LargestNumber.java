6e708398b2eabfa7d47aa1334eafd264ef8f00f5
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-20 14:23:26 +0530

    Basic Core programs part 2

---
+++ src/BasicCorePrograms/LargestNumber.java
@@ -0,0 +1,15 @@
+package BasicCorePrograms;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest = Math.max(a, Math.max(b, c));
+        System.out.println("Largest: " + largest);
+    }
+}
