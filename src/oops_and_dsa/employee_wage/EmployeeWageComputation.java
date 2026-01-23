c37c1b63e2d5cdfef4fe38a8a2ab6ac7a6f33289
Daanish Manivannan <daanish.manivannan@bridgelabz.com>
2026-01-23 17:32:18 +0530

    UC2 Done- Daily Wage calucation Added

--- src/oops_and_dsa/employee_wage/EmployeeWageComputation.java
+++ src/oops_and_dsa/employee_wage/EmployeeWageComputation.java
@@ -5,15 +5,21 @@ public class EmployeeWageComputation {
     private static final int IS_PRESENT = 1;
     private static final int IS_ABSENT = 0;
 
+    private static final int WAGE_PER_HOUR = 20;
+    private static final int FULL_DAY_HOURS = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int attendance = (int) (Math.random() * 2);
 
         if (attendance == IS_PRESENT) {
+            int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOURS;
             System.out.println("Employee is Present");
+            System.out.println("Daily Employee Wage: " + dailyWage);
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Employee Wage: 0");
         }
     }
 }
